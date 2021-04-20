9a9e670e73ebf991eda9ac8e8c6c4f6f5523cafc
lingoufan <1400695792@qq.com>
2021-04-20 17:22:26 +0800

    楼楼商城api项目初始化

---
+++ src/main/java/top/liekai/mallapi/MallApiApplication.java
@@ -0,0 +1,13 @@
+package top.liekai.mallapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/liekai/mallapi/MallApiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.liekai.mallapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
